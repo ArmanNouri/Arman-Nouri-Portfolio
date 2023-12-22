10e6ae84d3390ab92139a71f87a79cd5de825f96
ArmanNouri <openeyesn2y@gmail.com>
2023-12-22 13:30:23 +0100

    file js

---
+++ first.js
@@ -0,0 +1,38 @@
+      
+        /* NumeroUtente = prompt("inserisci un numero: ")
+
+        function Casuale() {
+
+            numeroCasuale = Math.ceil(Math.random()*NumeroUtente);
+            return alert(numeroCasuale)
+        }
+        console.log(Casuale())
+
+        num1= Number(prompt("inserisci primo numero: "))
+        num2= Number(prompt("inserisci secondo numero: "))
+        differenza = num1-num2;
+        alert("la differenza é " + differenza );
+        somma = num1+num2;
+        alert("la somma é " + somma ); */
+       
+        body =  document.body;
+        function stampa(x1,x2) {
+            body.append(x1,x2);
+            const br =document.createElement('br');
+            body.append(br);
+        }
+         stampa("Hola", "Hola2");
+         stampa("Hola", "hey");S
+
+        /* let n = prompt("inserisci un numero esadecimale")
+        let ndaconvertire = '0x$(n)'
+        let n2 = Number(ndaconvertire)
+        console.log(n2); */
+        /*
+        function differenza(num1, num2){
+            somma= num1 + num2;
+            return alert(somma)
+        }
+        console.log(differenza())
+        */
+       
